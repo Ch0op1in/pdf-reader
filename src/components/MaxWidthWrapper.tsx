fe56ae725c92b7900a0742e02e79af0ddb866dd7
$hopin <118271623+Ch0op1in@users.noreply.github.com>
2024-08-13 19:54:48 +0800

    maxWidth

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,6 +1,16 @@
+import { ReactNode } from "react"
 
-const MawWidthWrapper = () => {
+const MawWidthWrapper = ({
+    className,
+    children
+}: {
+    className?: string
+    children: ReactNode
     
+}) => {
+    <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
+        {children}
+    </div>
 }
 
 export default MawWidthWrapper  
