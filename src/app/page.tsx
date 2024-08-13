d6c8404d7127a7edae724dc3c75e51fcd35c3e50
$hopin <118271623+Ch0op1in@users.noreply.github.com>
2024-08-13 22:14:27 +0800

    landing page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,12 +52,57 @@ export default function Home() {
               </div>
             </div>
           </div>
-
-
-
         </div>
       </div>
+        
+        <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+            <div className="mb-12 px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl sm:text-center">
+                <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">Star Chatting</h2>
+                <p className="mt-4 text-lg text-gray-600">Learn and discover more from your PDFs</p>
+              </div>
+            </div>
 
+            <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+              <li className="md:flex-1">
+                <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+                  <span className="text-small font-medium text-blue-600 ">Step 1</span>
+                  <span className="text-xl font-semibold">Sign up for an account</span>
+                  <span className="mt-2 text-zinc-700">Start and try out reeead for free.</span>
+                </div>
+              </li>
+              <li className="md:flex-1">
+                <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+                  <span className="text-small font-medium text-blue-600 ">Step 2</span>
+                  <span className="text-xl font-semibold">Upload your PDF</span>
+                  <span className="mt-2 text-zinc-700">In seconds we'll process your PDF to be ready to go</span>
+                </div>
+              </li>
+              <li className="md:flex-1">
+                <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+                  <span className="text-small font-medium text-blue-600 ">Step 3</span>
+                  <span className="text-xl font-semibold">Start chatting</span>
+                  <span className="mt-2 text-zinc-700">Ask unlimited questions to your PDF.</span>
+                </div>
+              </li>
+
+            </ol>
+            
+            <div className="mx-auto max-w-6xl px-6 lg:px-8">
+              <div className="mt-16 flow-root sm:mt-24">
+                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                  <Image
+                  src="/file-upload-preview.jpg"
+                  alt="product"
+                  height={1419}
+                  width={732}
+                  quality={100}
+                  className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10 "/>
+                </div>
+              </div>
+            </div>
+
+        </div>
       </>
   );
 }
