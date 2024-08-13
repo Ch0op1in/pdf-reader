ca009c51e7c096733f1518e4bac15dcf25e02381
$hopin <118271623+Ch0op1in@users.noreply.github.com>
2024-08-13 20:14:12 +0800

    use pnpm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+ 
+      <MaxWidthWrapper className="bg-red-500"> 
+        <div>
+          <h1 className="text-white">Test</h1>
+        </div>
+      </MaxWidthWrapper>
   );
 }
--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -5,12 +5,13 @@ const MawWidthWrapper = ({
     children
 }: {
     className?: string
-    children: ReactNode
-    
+    children: ReactNode 
 }) => {
+    return (
     <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
         {children}
     </div>
+    )
 }
 
 export default MawWidthWrapper  
