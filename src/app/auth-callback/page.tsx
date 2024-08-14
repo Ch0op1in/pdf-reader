109b8fe3934e931afaf73d602012adb04bebd531
$hopin <118271623+Ch0op1in@users.noreply.github.com>
2024-08-14 19:25:24 +0800

    callbakc on point

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,14 +1,15 @@
-import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/router";
+"use client"
+import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/client";
 import { useEffect } from "react";
+import { Loader2 } from "lucide-react";
 
 const AuthCallback = () => {
     const router = useRouter();
     const searchParams = useSearchParams();
     const origin = searchParams.get('origin');
 
-    const { data, isLoading, isSuccess } = trpc.authCallback.useQuery(undefined);
+    const { data, isLoading, isSuccess, isError, error } = trpc.authCallback.useQuery(undefined);
 
     useEffect(() => {
         if (isSuccess && data?.success) {
@@ -16,10 +17,24 @@ const AuthCallback = () => {
         }
     }, [isSuccess, data, router, origin]);
 
-    // Tu peux gérer l'affichage pendant le chargement ou les erreurs ici si nécessaire
-    if (isLoading) return <p>Loading...</p>;
+    useEffect(() => {
+        if (isError) {
+            console.error("Erreur lors de l'authentification :", error);
+            // Gérer l'erreur ici, par exemple en redirigeant vers une page d'erreur
+            router.push("/error"); // Exemple de redirection vers une page d'erreur
+        }
+    }, [isError, error, router]);
 
-    return null; // Ou tout autre composant de rendu si besoin
+
+    return (
+        <div className="w-full mt-24 flex justify-center">
+            <div className="flex flex-col items-center gap-2">
+                <Loader2 className="h-8 w-8 text-zinc-800 animate-spin"/>
+                <h3 className="font-semibold text-xl">Setting up your account...</h3>
+                <p>You will be redirected automaticaly</p>
+            </div>
+        </div>
+    );
 };
 
 export default AuthCallback;
