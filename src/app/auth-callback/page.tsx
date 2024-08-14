500091a0a757a3bbb2ad9be2290bf9e86775f3ab
$hopin <118271623+Ch0op1in@users.noreply.github.com>
2024-08-14 18:47:21 +0800

    trcp config

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,13 +1,25 @@
 import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
+import { trpc } from "../_trpc/client";
+import { useEffect } from "react";
 
 const AuthCallback = () => {
     const router = useRouter();
     const searchParams = useSearchParams();
-    const origin = searchParams.get('origin')
+    const origin = searchParams.get('origin');
 
-    
+    const { data, isLoading, isSuccess } = trpc.authCallback.useQuery(undefined);
 
-}
+    useEffect(() => {
+        if (isSuccess && data?.success) {
+            router.push(origin ? `/${origin}` : "/dashboard");
+        }
+    }, [isSuccess, data, router, origin]);
 
-export default AuthCallback
+    // Tu peux gérer l'affichage pendant le chargement ou les erreurs ici si nécessaire
+    if (isLoading) return <p>Loading...</p>;
+
+    return null; // Ou tout autre composant de rendu si besoin
+};
+
+export default AuthCallback;
