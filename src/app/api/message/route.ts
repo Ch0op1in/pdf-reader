79ff5fb38656304f04045bd5f1081fe34dba6f52
$hopin <118271623+Ch0op1in@users.noreply.github.com>
2024-08-17 14:12:34 +0800

    call chatgpt stream function

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -2,6 +2,11 @@ import { db } from "@/db";
 import { messageValidator } from "@/lib/validators/SendMessageValidator";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { Pinecone } from "@pinecone-database/pinecone";
+import { PineconeStore } from "@langchain/pinecone";
+import { openai } from "@/lib/openai";
+
 
 
 export const POST = async (req: NextRequest) => {
@@ -37,4 +42,73 @@ export const POST = async (req: NextRequest) => {
             fileId
         }
     })
+
+    // Vectorize message 
+
+
+    const embeddings = new OpenAIEmbeddings({
+        apiKey: process.env.OPENAI_API_KEY, 
+      })
+
+    const pinecone = new Pinecone
+
+    const pineconeIndex = pinecone.Index("reeead")
+
+    const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+        pineconeIndex,
+        namespace: file.id
+    })
+
+    const results = await vectorStore.similaritySearch(message, 4)
+
+    const prevMessages = await db.message.findMany({
+        where:{
+            fileId
+        },
+        orderBy:{
+            createdAt: 'asc'
+        },
+        take: 6
+    })
+
+    const formattedPrevMessages = prevMessages.map((msg) => ({
+        role: msg.isUserMessage ? "user" as const : "assistant" as const,
+        content: msg.text 
+    }))
+
+    const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        temperature: 0,
+        stream: true,
+        messages: [
+          {
+            role: 'system',
+            content:
+              'Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.',
+          },
+          {
+            role: 'user',
+            content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
+            
+      \n----------------\n
+      
+      PREVIOUS CONVERSATION:
+      ${formattedPrevMessages.map((message) => {
+        if (message.role === 'user')
+          return `User: ${message.content}\n`
+        return `Assistant: ${message.content}\n`
+      })}
+      
+      \n----------------\n
+      
+      CONTEXT:
+      ${results.map((r) => r.pageContent).join('\n\n')}
+      
+      USER INPUT: ${message}`,
+          },
+        ],
+      })
+
+
+
 }
