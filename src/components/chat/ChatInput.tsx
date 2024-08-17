37aa867ed44386feae6a1f229dd313974f54311c
$hopin <118271623+Ch0op1in@users.noreply.github.com>
2024-08-17 10:38:35 +0800

    chatinput

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,15 +1,26 @@
+import { Input } from "../ui/input"
 
 
 interface ChatInputProps{
     isDisabled?: boolean
 }
 
-const ChatWrapper = ({isDisabled} : ChatInputProps) => {
+const ChatInput = ({isDisabled} : ChatInputProps) => {
     return(
-        <div>
+        <div className="absolut bottom-0 left-0 w-full">
+            <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+                <div className="relative flex h-full flex-1">
+                    <div className="relative flex flex-col w-full flex-grow p-4">
+                        <div className="relative">
+                            
+                        </div>
+                    </div>
+                </div>
+
+            </form>
             
         </div>
     )
 }
 
-export default ChatWrapper
+export default ChatInput
