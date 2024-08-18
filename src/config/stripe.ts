2eb0b9b4b1b473bf4a432052f12ae1254cd0f9af
$hopin <118271623+Ch0op1in@users.noreply.github.com>
2024-08-18 15:45:58 +0800

    stripe config

---
+++ src/config/stripe.ts
@@ -0,0 +1,29 @@
+const Plan = [
+    {
+        name: "Free",
+        slug:"free",
+        quota: 10,
+        pagesPerPDF: 5,
+        price: {
+            amount: 0,
+            priceIds: {
+                test: '',
+                production: ''
+            }
+        }
+    },
+    {
+        name: "Pro",
+        slug:"pro",
+        quota: 50,
+        pagesPerPDF: 25,
+        price: {
+            amount: 19,
+            priceIds: {
+                test: 'price_1Pp3jhKdhtoKBTiZSzLiynjC',
+                production: 'price_1Pp3jhKdhtoKBTiZSzLiynjC'
+            }
+        }
+    }
+    
+]
