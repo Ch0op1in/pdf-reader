a314f7572d6c17b947c876629435e40d35452939
$hopin <118271623+Ch0op1in@users.noreply.github.com>
2024-08-18 23:00:26 +0800

    final commit ?

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,60 +1,53 @@
-import ChatWrapper from "@/components/chat/ChatWrapper"
-import PdfRenderer from "@/components/PdfRenderer"
-import { db } from "@/db"
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
-import { notFound, redirect } from "next/navigation"
-import dynamic from 'next/dynamic';
-
-// Désactiver le SSR pour le composant PdfViewer
-const PdfViewerComponent = dynamic(() => import("@/components/PdfRenderer"), { 
-  ssr: false,
-});
+import ChatWrapper from '@/components/chat/ChatWrapper'
+import PdfRenderer from '@/components/PdfRenderer'
+import { db } from '@/db'
+import { getUserSubscriptionPlan } from '@/lib/stripe'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { notFound, redirect } from 'next/navigation'
 
 interface PageProps {
-    params: {
-        fileid: string
-    }
+  params: {
+    fileid: string
+  }
 }
 
-const filePage = async ({params} : PageProps) => {
+const Page = async ({ params }: PageProps) => {
+  const { fileid } = params
 
-    const {fileid} = params
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
 
-    const { getUser } = getKindeServerSession()
-    const user = await getUser()
+  if (!user || !user.id)
+    redirect(`/auth-callback?origin=dashboard/${fileid}`)
 
-    if(!user || !user.id){
-        redirect(`/auth-callback?origin=dashboard/${fileid}`)
-    }
+  const file = await db.file.findFirst({
+    where: {
+      id: fileid,
+      userId: user.id,
+    },
+  })
 
-    const file = await db.file.findFirst({
-        where:{
-            id: fileid,
-            userId : user.id
-        }
-    })
+  if (!file) notFound()
 
-    if(!file){
-        notFound()
-    }
+  const plan = await getUserSubscriptionPlan()
 
-    return (
-        <div className='flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]'>
+  return (
+    <div className='flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]'>
       <div className='mx-auto w-full max-w-8xl grow lg:flex xl:px-2'>
         {/* Left sidebar & main wrapper */}
         <div className='flex-1 xl:flex'>
           <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
             {/* Main area */}
-            <PdfViewerComponent url={file.url} />
+            <PdfRenderer url={file.url} />
           </div>
         </div>
 
         <div className='shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0'>
-          <ChatWrapper fileId={file.id} />
+          <ChatWrapper isSubscribed={plan.isSubscribed} fileId={file.id} />
         </div>
       </div>
     </div>
-    )
+  )
 }
 
-export default filePage
+export default Page
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,50 +1,57 @@
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { privateProcedure, publicProcedure, router } from './trpc';
-import { TRPCError } from '@trpc/server';
-import { db } from '@/db';
-import z from "zod"
-import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query';
-import { absoluteUrl } from '@/lib/utils';
-import { getUserSubscriptionPlan, stripe } from '@/lib/stripe';
-import { Plan } from '@/config/stripe';
- 
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import {
+  privateProcedure,
+  publicProcedure,
+  router,
+} from './trpc'
+import { TRPCError } from '@trpc/server'
+import { db } from '@/db'
+import { z } from 'zod'
+import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query'
+import { absoluteUrl } from '@/lib/utils'
+import {
+  getUserSubscriptionPlan,
+  stripe,
+} from '@/lib/stripe'
+import { Plan } from '@/config/stripe'
+
 export const appRouter = router({
-  authCallback : publicProcedure.query(async () => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession()
     const user = await getUser()
 
-    if(!user || !user.email){
-      throw new TRPCError({code: "UNAUTHORIZED"})
-    }
+    if (!user?.id || !user?.email)
+      throw new TRPCError({ code: 'UNAUTHORIZED' })
 
+    // check if the user is in the database
     const dbUser = await db.user.findFirst({
-      where:{
-        id: user.id
-      }
+      where: {
+        id: user.id,
+      },
     })
 
-    if(!dbUser){
+    if (!dbUser) {
+      // create user in db
       await db.user.create({
         data: {
           id: user.id,
-          email: user.email
-        }
+          email: user.email,
+        },
       })
     }
 
-    return {success: true}
+    return { success: true }
   }),
-  getUserFiles: privateProcedure.query(async ({ctx}) => {
-    const {userId, user} = ctx
+  getUserFiles: privateProcedure.query(async ({ ctx }) => {
+    const { userId } = ctx
 
     return await db.file.findMany({
       where: {
-        userId
-      }
+        userId,
+      },
     })
   }),
 
-  
   createStripeSession: privateProcedure.mutation(
     async ({ ctx }) => {
       const { userId } = ctx
@@ -103,120 +110,111 @@ export const appRouter = router({
     }
   ),
 
-
   getFileMessages: privateProcedure
-  .input(
-    z.object({
-      limit: z.number().min(1).max(100).nullish(),
-      cursor: z.string().nullish(),
-      fileId: z.string(),
-    })
-  )
-  .query(async ({ ctx, input }) => {
-    const { userId } = ctx
-    const { fileId, cursor } = input
-    const limit = input.limit ?? INFINITE_QUERY_LIMIT
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).nullish(),
+        cursor: z.string().nullish(),
+        fileId: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { userId } = ctx
+      const { fileId, cursor } = input
+      const limit = input.limit ?? INFINITE_QUERY_LIMIT
 
-    const file = await db.file.findFirst({
-      where: {
-        id: fileId,
-        userId,
-      },
-    })
+      const file = await db.file.findFirst({
+        where: {
+          id: fileId,
+          userId,
+        },
+      })
 
-    if (!file) throw new TRPCError({ code: 'NOT_FOUND' })
+      if (!file) throw new TRPCError({ code: 'NOT_FOUND' })
 
-    const messages = await db.message.findMany({
-      take: limit + 1,
-      where: {
-        fileId,
-      },
-      orderBy: {
-        createdAt: 'desc',
-      },
-      cursor: cursor ? { id: cursor } : undefined,
-      select: {
-        id: true,
-        isUserMessage: true,
-        createdAt: true,
-        text: true,
-      },
-    })
+      const messages = await db.message.findMany({
+        take: limit + 1,
+        where: {
+          fileId,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+        cursor: cursor ? { id: cursor } : undefined,
+        select: {
+          id: true,
+          isUserMessage: true,
+          createdAt: true,
+          text: true,
+        },
+      })
 
-    let nextCursor: typeof cursor | undefined = undefined
-    if (messages.length > limit) {
-      const nextItem = messages.pop()
-      nextCursor = nextItem?.id
-    }
+      let nextCursor: typeof cursor | undefined = undefined
+      if (messages.length > limit) {
+        const nextItem = messages.pop()
+        nextCursor = nextItem?.id
+      }
 
-    return {
-      messages,
-      nextCursor,
-    }
-  }),
+      return {
+        messages,
+        nextCursor,
+      }
+    }),
 
   getFileUploadStatus: privateProcedure
-  .input(z.object({ fileId: z.string() }))
-  .query(async ({ input, ctx }) => {
-    const file = await db.file.findFirst({
-      where: {
-        id: input.fileId,
-        userId: ctx.userId,
-      },
-    })
+    .input(z.object({ fileId: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const file = await db.file.findFirst({
+        where: {
+          id: input.fileId,
+          userId: ctx.userId,
+        },
+      })
 
-    if (!file) return { status: 'PENDING' as const }
+      if (!file) return { status: 'PENDING' as const }
 
-    return { status: file.uploadStatus }
-  }),
+      return { status: file.uploadStatus }
+    }),
 
   getFile: privateProcedure
-  .input(z.object({key: z.string()}))
-  .mutation(async ({ctx, input}) => {
-    const { userId } = ctx
+    .input(z.object({ key: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx
 
-    const file = await db.file.findFirst({
-      where:{
-        key: input.key,
-        userId
-      }
-    })
+      const file = await db.file.findFirst({
+        where: {
+          key: input.key,
+          userId,
+        },
+      })
 
-    if(!file){
-      throw new TRPCError({ code : 'NOT_FOUND'})
-    }
+      if (!file) throw new TRPCError({ code: 'NOT_FOUND' })
 
-    return file
-  }),
+      return file
+    }),
 
-  deleteFile: privateProcedure.input(
-    z.object({id : z.string()})
-  ).mutation( async ({ctx, input}) => {
-    const {userId} = ctx
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx
 
-    const file = await db.file.findFirst({
-      where: {
-        id : input.id,
-        userId
-      }
-    })
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId,
+        },
+      })
 
-    if (!file){
-      throw new TRPCError({code : "NOT_FOUND"})
-    }
+      if (!file) throw new TRPCError({ code: 'NOT_FOUND' })
 
-    await db.file.delete({
-      where: {
-        id: input.id,
+      await db.file.delete({
+        where: {
+          id: input.id,
+        },
+      })
 
-      }
-    })
+      return file
+    }),
+})
 
-    return file
-  })
-
-
-});
- 
-
-export type AppRouter = typeof appRouter;
+export type AppRouter = typeof appRouter
